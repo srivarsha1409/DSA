d2c885a61c3c321bf650b462cc5dd9ff79f02bd8
srivarsha1409 <srivarshagkgk@gmail.com>
2025-08-22 22:38:43 +0530

    Add notebook

---
+++ day1.py
@@ -0,0 +1,21 @@
+#code 1:input/output
+a=int(input("Input:"))
+if -1000 <= a <= 1000:      
+   print("Output:",a)
+else:
+    print("Error")
+
+
+#code 2:datatypes
+b=int(input("Enter number:"))
+print("Output:",b)
+print(type(b))
+
+c=input("Enter string:")
+print("Output:",c)
+print(type(c))
+
+d=float(input("Enter decimal:"))
+print("Output:",d)
+print(type(d))
+
